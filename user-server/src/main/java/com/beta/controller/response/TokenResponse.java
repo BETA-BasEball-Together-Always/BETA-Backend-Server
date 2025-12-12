55aacc080470251f4e76952fe4c158ce5dd2b7a9
노영오 <nohyo05@naver.com>
2025-12-12 23:18:08 +0900

    인증 관련 DTO 및 Request/Response 클래스 추가
      DTO:
      - TokenDto: accessToken, refreshToken을 담는 DTO
    
      Request:
      - EmailLoginRequest: 이메일 로그인 요청 (이메일, 비밀번호)
      - RefreshTokenRequest: 토큰 갱신 요청
    
      Response:
      - DuplicateResponse: 중복 확인 응답
      - LogoutResponse: 로그아웃 응답
      - TokenResponse: 토큰 응답

---
+++ user-server/src/main/java/com/beta/controller/response/TokenResponse.java
@@ -0,0 +1,19 @@
+package com.beta.controller.response;
+
+import com.beta.account.application.dto.TokenDto;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class TokenResponse {
+    private String accessToken;
+    private String refreshToken;
+
+    public static TokenResponse from(TokenDto tokenDto) {
+        return TokenResponse.builder()
+                .accessToken(tokenDto.getAccessToken())
+                .refreshToken(tokenDto.getRefreshToken())
+                .build();
+    }
+}
