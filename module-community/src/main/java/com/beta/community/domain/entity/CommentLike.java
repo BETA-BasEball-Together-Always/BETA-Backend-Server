d43729e97244a41e7e3d7bbe89dfca44fad6e314
노영오 <nohyo05@naver.com>
2026-02-19 18:13:26 +0900

    CommentLike 엔티티 unique constraint 추가
    - userId, commentId 복합 unique 제약조건 추가

--- module-community/src/main/java/com/beta/community/domain/entity/CommentLike.java
+++ module-community/src/main/java/com/beta/community/domain/entity/CommentLike.java
@@ -4,6 +4,7 @@ import com.beta.core.domain.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,7 +12,9 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
-@Table(name = "comment_like")
+@Table(name = "comment_like", uniqueConstraints = {
+        @UniqueConstraint(name = "uk_comment_like_user_comment", columnNames = {"user_id", "comment_id"})
+})
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CommentLike extends BaseEntity {
 
